6a985b3f1e68e9d0a96ced8ec9747924e7d1c02a
marvieiradev <marcosvieira482@gmail.com>
2025-01-10 22:01:59 -0300

    feat: ensure that team equipment is shown even if it is not available in the API

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -5,6 +5,8 @@ const Team = () => {
   const [teamName, setTeamName] = useState("");
   const [teamBadge, setTeamBadge] = useState("");
   const [teamEquipment, setTeamEquipment] = useState("");
+  const [teamColor1, setTeamColor1] = useState("");
+  const [teamColor2, setTeamColor2] = useState("");
   const params = useParams();
   const team = params.id;
 
@@ -19,6 +21,8 @@ const Team = () => {
           setTeamName(json.teams[0].strTeam);
           setTeamBadge(json.teams[0].strBadge);
           setTeamEquipment(json.teams[0].strEquipment);
+          setTeamColor1(json.teams[0].strColour1);
+          setTeamColor2(json.teams[0].strColour2);
         })
         .catch((error) => console.log(error));
     };
@@ -27,10 +31,31 @@ const Team = () => {
 
   return (
     <div className="flex flex-col">
-      <h1>{teamName}</h1>
-      <div className="flex">
-        <img src={teamBadge} className="w-[200px]" alt="" />
-        <img src={teamEquipment} className="w-[200px]" alt="" />
+      <div
+        className="flex h-[50px] justify-center items-center"
+        style={{ backgroundColor: teamColor1, color: teamColor2 }}
+      >
+        <h1 className="drop-shadow-lg text-center font-bold text-2xl">
+          {teamName}
+        </h1>
+      </div>
+      <div className="flex justify-between px-6">
+        <img src={teamBadge} className="w-[150px] p-1" alt="" />
+
+        {teamEquipment ? (
+          <div>
+            <img src={teamEquipment} className="w-[150px]" alt="" />
+          </div>
+        ) : (
+          <div className="relative">
+            <img
+              className="absolute index-1 w-[22px] top-10 right-10"
+              src={teamBadge}
+              alt=""
+            />
+            <img src="/no-equip.png" className="w-[150px]" alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
